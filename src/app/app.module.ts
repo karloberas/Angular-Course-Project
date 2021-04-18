997a29d12fdf2e06f655ca3f2143592340cc375a
Karl John <7484848+karloberas@users.noreply.github.com>
2021-04-18 22:06:15 +0800

    Clean-up and added debugging tools for ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,23 +13,22 @@ import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
-
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-  ],
-  imports: [
-      BrowserModule,
-      AppRoutingModule,
-      StoreModule.forRoot(fromApp.appReducer),
-      EffectsModule.forRoot([AuthEffects]),
-      HttpClientModule,
-      SharedModule,
-      CoreModule
+    declarations: [AppComponent, HeaderComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        StoreModule.forRoot(fromApp.appReducer),
+        EffectsModule.forRoot([AuthEffects]),
+        StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+        StoreRouterConnectingModule.forRoot(),
+        HttpClientModule,
+        SharedModule,
+        CoreModule,
     ],
-  bootstrap: [AppComponent],
-  // providers: [LoggingService]
+    bootstrap: [AppComponent],
+    // providers: [LoggingService]
 })
 export class AppModule {}
